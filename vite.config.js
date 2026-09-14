d2f1f114ffde08abfa70d0d2c2b55c0be91f9b0d
govarathan <govarathan02@gmail.com>
2026-09-14 20:14:37 +0530

    Add Vercel configuration vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sri-pattu-zari-hub/', 
-})
+  base:
+    process.env.VERCEL || process.env.VERCEL_ENV ? "/" : "/sri-pattu-zari-hub/",
+});
